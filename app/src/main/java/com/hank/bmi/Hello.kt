87940aef82ce80dfd67b8676650cdcf8d4d5b5a1
hank2 <hank2003168@gmail.com>
2025-01-22 13:38:00 +0800

    initial commit

---
+++ app/src/main/java/com/hank/bmi/Hello.kt
@@ -0,0 +1,14 @@
+package com.hank.bmi
+//類別-(建置)-物件
+class Hello {
+}
+//First level
+fun main() {
+    println("Hello kotlin")
+    val age=20  //val:定值 can't reassigned
+    println( age.inc())//資料.方法 inc()=資料+1
+    val name="HANK"
+    println(name.get(2))
+    var number=200; //var:變數
+
+}
