69262db22d461b9cce56ed3ca539a5c42cf1d5f9
hank2 <hank2003168@gmail.com>
2025-01-22 15:25:42 +0800

    Constructor

--- app/src/main/java/com/hank/bmi/Hello.kt
+++ app/src/main/java/com/hank/bmi/Hello.kt
@@ -1,23 +1,23 @@
 package com.hank.bmi
+
+import androidx.compose.ui.Modifier
+
 //class-(build)-object
 class Hello {
 }
 //First level
 fun main() {
-    p.hello()
+    val p=Person("HANK",65.5f,1.7f)   //建立了一個 Person 類別的實例 p
+    val jack=Person("jack",65f,2.4f)
+    println(jack.name+"s"+"BMI= "+jack.bmi())  //存取 jack.name 獲取名字
+//    p.weight=65.5f
+//    p.height=1.7f
+    println("BMI= "+p.bmi())
 
 
 
 
-
-
-
-
-
-
-
-
-//    val p=Person()
+//    p.hello()
 //    println("Hello kotlin")
 //    val age=20  //val:定值 can't reassigned
 //    println( age.inc())//資料.方法 inc()=資料+1
--- app/src/main/java/com/hank/bmi/Person.kt
+++ app/src/main/java/com/hank/bmi/Person.kt
@@ -1,8 +1,14 @@
 package com.hank.bmi
-
-class Person {
+import androidx.compose.ui.Modifier
+class Person constructor(val name:String, val weight:Float, val height:Float) {
+//    var weight:Float=0f//屬性
+//    var height:Float=0f//屬性
+    fun bmi():Float //回傳一個浮點數
+    {
+     val bmi=weight/(height*height)//bmi local variable
+        return bmi  //回傳結果
+    }
     fun hello(){
     println("Hello")
-
     }
 }
